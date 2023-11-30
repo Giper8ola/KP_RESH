72d2e297ee5061554feb7c3c122579df136513c5
Giper8ola <matwey.peregudow2100@gmail.com>
2023-11-30 18:43:43 +0300

    30.11.2023

--- KP/source.c
+++ KP/source.c
@@ -1,6 +1,7 @@
 
 
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
@@ -8,132 +9,313 @@
 #include <time.h>
 #include <Windows.h>
 
-typedef struct Zakaz
+
+typedef struct Date
 {
-    int number;
-    char date[20];
-    char client[100];
-    int summ;
-    int status;
-} z;
+    int day;
+    int month;
+    int year;
+} date;
 
-int save(char* filename, struct Zakaz zakaz[1]);
+typedef struct Inventory
+{
+    char name[100];
+    char category[100];
+    int  number;
+    char description[100];
+    date Date;
+    char state[100];
+} inventory;
+
+int save(char* filename, inventory Invent[1]);
 int loadMany(char* filename);
-int loadOne(char* filename, int number);
-int sort(char* fileName, char date[20]);
+int loadOne(char* filename, char category[50]);
+int sort(char* fileName);
+int updateOne(char* fileName, int number, inventory newParams);
 
-void main()
+int compare(const void* x1, const void* x2);
+inventory* fillArrayOfInv(char* filename);
+int countOfInvInFile(char* fileName);
+
+int main()
 {
     int state;
     srand(time(NULL));
     system("chcp 1251");
     setlocale(LC_ALL, "RUS");
     char* filename = "data.txt";
-	do
-	{
-		printf("\n1 - Загрузить заказ в БД\n2 - Получить заказы из БД\n3 - Найти заказ в БД\n4 - Отосортировать БД\n5 - Изменить заказ\n");
-		scanf("%d", &state);
-		
-		switch (state)
-		{
-			case 1: {
-                struct Zakaz zakaz[1];
-                char firstName[48], lastName[48], name[100] = "";
-                zakaz[0].number = rand() % 100;
-				printf("Введите дату заказа: ");
-                scanf("%s", &zakaz[0].date);
-                printf("Введите имя и фамилию клиента: ");
-                scanf("%s %s", &firstName, &lastName);
-                strcat(name, firstName);
-                strcat(name, " ");
-                strcat(name, lastName);
-                strncpy(zakaz[0].client, name, 100);
-                printf("Ввседите сумму заказа: ");
-                scanf("%d", &zakaz[0].summ);
-                printf("Укажите статус заказа: ");
-                scanf("%d", &zakaz[0].status);
-                save(filename, zakaz);
-                break;
-			}
-            case 2: {
-                loadMany(filename);
-                break;
-            }
-            case 3: {
-                int number;
-                printf("Ввдите номер заказа: ");
-                scanf("%d", &number);
-                loadOne(filename, number);
-                break;
-            }
-            case 4: {
-                char arr[20] = "fddss";
-                sort(filename, arr);
-            }
-            
-		}
-		
-	} while (1);
+    do
+    {
+        printf("\n1 - Загрузить заказ в БД\n2 - Получить заказы из БД\n3 - Найти заказ в БД\n4 - Отосортировать БД\n5 - Изменить заказ\n");
+        scanf("%d", &state);
+
+        switch (state)
+        {
+        case 1: {
+            inventory invent[1];
+
+            invent[0].number = rand() % 100;
+
+            printf("Введите название оборудования: ");
+            fgets(invent[0].name, 100, stdin);
+            scanf("%[^\n]", &invent[0].name);
+
+            printf("Введите категорию оборудования: ");
+            fgets(invent[0].category, 100, stdin);
+            scanf("%[^\n]", &invent[0].category);
+
+            printf("Введите описания оборудования: ");
+            fgets(invent[0].description, 100, stdin);
+            scanf("%[^\n]", &invent[0].description);
+
+            printf("Дата инвертаризации: \n");
+            printf("Введите день: ");
+            scanf("%d", &invent[0].Date.day);
+            printf("Введите месяц: ");
+            scanf("%d", &invent[0].Date.month);
+            printf("Введите год: ");
+            scanf("%d", &invent[0].Date.year);
+
+            printf("Введите состояния оборудования: ");
+            fgets(invent[0].state, 100, stdin);
+            scanf("%[^\n]", &invent[0].state);
+
+            save(filename, invent);
+            break;
+        }
+        case 2: {
+            loadMany(filename);
+            break;
+        }
+
+        case 3: {
+            char category[100];
+            printf("Введите категорию заказа: ");
+            fgets(category, 100, stdin);
+            scanf("%[^\n]", &category);
+            loadOne(filename, category);
+            break;
+        }
+
+        case 4: {
+            puts("По дате инвертаризации: ");
+            sort(filename);
+            break;
+        }
+
+        case 5:
+        {
+            int state2 = 1, number, state3;
+            inventory newParams = { "", "", 100, "", {0, 0, 0}, "" };
+            printf("Введите номер заказа: ");
+            scanf("%d", &number);
+
+            do {
+                printf("\n1 - Изменить название\n2 - Изменить категорию\n3 - Изменить описание\n4 - Изменить дату\n5 - Изменить состояние\n6 - Выполнить изменения\n");
+                scanf("%d", &state3);
+
+                switch (state3) {
+                case 1:
+                {
+                    printf("Введите новое название оборудования: ");
+                    fgets(newParams.name, 100, stdin);
+                    scanf("%[^\n]", &newParams.name);
+                    break;
+                }
+                case 2:
+                {
+                    printf("Введите новую категорию оборудования: ");
+                    fgets(newParams.category, 100, stdin);
+                    scanf("%[^\n]", &newParams.category);
+                    break;
+                }
+                case 3:
+                {
+                    printf("Введите новое описание оборудования: ");
+                    fgets(newParams.description, 100, stdin);
+                    scanf("%[^\n]", &newParams.description);
+                    break;
+                }
+                case 4:
+                {
+                    printf("Новая дата инвертаризации: \n");
+                    printf("Введите день: ");
+                    scanf("%d", &newParams.Date.day);
+                    printf("Введите месяц: ");
+                    scanf("%d", &newParams.Date.month);
+                    printf("Введите год: ");
+                    scanf("%d", &newParams.Date.year);
+                    break;
+                }
+                case 5:
+                {
+                    printf("Введите новое состояние оборудования: ");
+                    fgets(newParams.state, 100, stdin);
+                    scanf("%[^\n]", &newParams.state);
+                    break;
+                }
+                case 6:
+                {
+                    updateOne(filename, number, newParams);
+                    state2 = 2;
+                    break;
+                }
+                }
+            } while (state2 == 1);
+        }
+        default:
+            break;
+        }
+
+    } while (1);
 }
 
-int save(char* filename, struct Zakaz zakaz[1])
+int save(char* filename, inventory Invent[1])
 {
-    int size = sizeof(zakaz[0]);              // размер всего массива
-    int count = sizeof(zakaz) / size;         // количество структур 
-
+    size_t size = sizeof Invent[0];
     FILE* fp = fopen(filename, "a+");
-    size_t written = fwrite(zakaz, size, 1, fp);
+    fwrite(Invent, size, 1, fp);
     printf("Заказ успено сохранен");
     fclose(fp);
 }
 
 int loadMany(char* filename)
 {
-    struct Zakaz p; 
+    inventory Invent;
     FILE* fp = fopen(filename, "r");
-    printf("\nЗаказы в базе данных: \n");
-    while (fread(&p, sizeof(p), 1, fp) == 1)
+    printf("Заказы в базе данных: \n");
+    while (fread(&Invent, sizeof(Invent), 1, fp) == 1)
     {
-        printf("\nId: %d\nDate: %s\nClient: %s\nСумма заказа: %d\nСтатус заказа: %d", p.number, p.date, p.client, p.summ, p.status);
+        printf("\nНазвание оборудования: %s\nКатегория оборудования: %s\nНомер оборудования: %d\nОписание оборудования: %s\nДата инвертаризации: %d.%d.%d\nСостояние оборудования: %s\n", Invent.name, Invent.category, Invent.number, Invent.description, Invent.Date.day, Invent.Date.month, Invent.Date.year, Invent.state);
     }
     printf("\n");
     fclose(fp);
 }
 
-int loadOne(char* filename, int number)
+
+int loadOne(char* filename, char category[50])
 {
-    struct Zakaz p;
+    inventory Invent;
     FILE* fp = fopen(filename, "r");
-    printf("\nЗаказ по номером: %d\n", number);
-    while (fread(&p, sizeof(p), 1, fp) == 1)
+    printf("\nЗаказы категории: %s\n", category);
+    while (fread(&Invent, sizeof(Invent), 1, fp) == 1)
     {
-        if (p.number == number)
-            printf("\nId: %d\nDate: %s\nClient: %s\nСумма заказа: %d\nСтатус заказа: %d", p.number, p.date, p.client, p.summ, p.status);
+        if (strcmp(Invent.category, category) == 0)
+            printf("\n\tНазвание оборудования: %s\n\tКатегория оборудования: %s\n\tНомер оборудования: %d\n\tОписание оборудования: %s\n\tДата инвертаризации: %d.%d.%d\n\tСостояние оборудования: %s\n", Invent.name, Invent.category, Invent.number, Invent.description, Invent.Date.day, Invent.Date.month, Invent.Date.year, Invent.state);
     }
-    printf("\n");
     fclose(fp);
 }
 
-int sort(char* fileName, char date[20])
+int sort(char* fileName)
 {
-    struct Zakaz p;
+    inventory* Invent;
+    Invent = fillArrayOfInv(fileName);
+
+    int j2 = countOfInvInFile(fileName);
+
+    qsort(Invent, j2, sizeof(inventory), compare);
+
+    for (int i = 0; i < j2; i++)
+        printf("\n\tНазвание оборудования: %s\n\tКатегория оборудования: %s\n\tНомер оборудования: %d\n\tОписание оборудования: %s\n\tДата инвертаризации: %d.%d.%d\n\tСостояние оборудования: %s\n", Invent[i].name, Invent[i].category, Invent[i].number, Invent[i].description, Invent[i].Date.day, Invent[i].Date.month, Invent[i].Date.year, Invent[i].state);
+
+    printf("\n");
+}
+
+int updateOne(char* fileName, int number, inventory newParams)
+{
+    int pos;
+    inventory* Invent;
+    inventory inv;
+    inventory* newInvent;
+
+    Invent = fillArrayOfInv(fileName);
+
+    int j2 = countOfInvInFile(fileName);
+
+    newInvent = (inventory*)malloc(j2 * sizeof(inventory));
+
+    for (int i = 0; i < j2; i++)
+    {
+        if (Invent[i].number == number)
+        {
+            pos = i;
+            continue;
+        }
+        newInvent[i] = Invent[i];
+    }
+
     FILE* fp = fopen(fileName, "r");
-    fseek(fp, 0, SEEK_END);
-    int file_size = ftell(fp);
-    int n = file_size / sizeof(p);
-    struct Zakaz* zakaz;
-    zakaz = malloc(n * sizeof(struct Zakaz));
-
-    int i = 0;
-    while (fread(&p, sizeof(p), 1, fp) == 1 && i < n)
+    while (fread(&inv, sizeof(inv), 1, fp) == 1)
     {
-        *(zakaz + n + i) = p;
-        i++;
+        if (inv.number == number)
+        {
+            strcpy(inv.name, strcmp(newParams.name, "") != 0 ? newParams.name : inv.name);
+            strcpy(inv.description, strcmp(newParams.description, "") != 0 ? newParams.description : inv.description);
+            strcpy(inv.category, strcmp(newParams.category, "") != 0 ? newParams.category : inv.category);
+            strcpy(inv.state, strcmp(newParams.state, "") != 0 ? newParams.state : inv.state);
+            if (newParams.Date.day + newParams.Date.month + newParams.Date.year != 0)
+            {
+                inv.Date.day = newParams.Date.day;
+                inv.Date.month = newParams.Date.month;
+                inv.Date.year = newParams.Date.year;
+            }
+            break;
+        }
     }
-    for (int i = 0; i < n; i++)
-        printf("\nId: %d", (*(zakaz + n + i)).number);
 
-    printf("\n");
     fclose(fp);
+    newInvent[pos] = inv;
+
+    size_t size = sizeof(newInvent[0]);
+    FILE* fp2 = fopen(fileName, "w+");
+    fwrite(newInvent, size, j2, fp2);
+    printf("Файл успешно обновлен");
+    fclose(fp2);
 }
 
+int compare(const void* x1, const void* x2)
+{
+    inventory* A = (inventory*)x1;
+    inventory* B = (inventory*)x2;
+    if (A->Date.year == B->Date.year) {
+        if (A->Date.month == B->Date.month)
+            return A->Date.day - B->Date.day;
+        else
+            return A->Date.month - B->Date.month;
+    }
+    else
+        return A->Date.year - B->Date.year;
+}
+
+int countOfInvInFile(char* fileName)
+{
+    int count = 0;
+    FILE* fp = fopen(fileName, "r");
+    inventory inv;
+    while (fread(&inv, sizeof(inv), 1, fp) == 1)
+    {
+        count++;
+    }
+    fclose(fp);
+    return count;
+}
+
+inventory* fillArrayOfInv(char* fileName)
+{
+    int j, j2 = 0;
+    inventory inv;
+    inventory* Invent;
+    j = countOfInvInFile(fileName);
+
+    FILE* fp2 = fopen(fileName, "r");
+    Invent = (inventory*)malloc(j * sizeof(inventory));
+
+    while (fread(&inv, sizeof(inv), 1, fp2) == 1)
+    {
+        Invent[j2] = inv;
+        j2++;
+    }
+    fclose(fp2);
+
+    return Invent;
+}
